14ec103b10d610132dffac9d979d1352aee7751a
robertobuso <robertobuso@gmail.com>
2018-11-11 11:37:52 -0500

    first commit

---
+++ app/controllers/api/v1/trees_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::TreesController < ApplicationController
+  before_action :find_tree, only: [:update]
+
+  def show
+  end
+  
+  def index
+    @trees = Tree.all
+    render json: @trees
+  end
+
+  def update
+    @tree.update(tree_params)
+    if @tree.save
+      render json: @tree, status: :accepted
+    else
+      render json: { errors: @tree.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def tree_params
+    params.permit(:title, :content)
+  end
+
+  def find_tree
+    @tree = Tree.find(params[:id])
+  end
+end
